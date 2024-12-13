a3a47d371eb433a030f60865da81aaceb3b10c5b
JenikP <95909702+JenikP@users.noreply.github.com>
2024-12-13 11:43:39 +1100

    Update beautyServices.ts

--- src/data/services/beautyServices.ts
+++ src/data/services/beautyServices.ts
@@ -18,27 +18,27 @@ export const beautyAddOns = [
     price: "from $20",
     description: "Darker, more defined lashes",
     duration: "20 mins",
-    image: "https://media.istockphoto.com/photos/beauty-treatment-curling-eyelashes-by-using-a-curler-lash-lamination-picture-id1074129756?k=20&m=1074129756&s=612x612&w=0&h=ZhMKMFDf2QMWzfxgNXANoU2EfMQqD8oHYOThfzevwbA="
+    image: "https://t4.ftcdn.net/jpg/06/68/57/55/240_F_668575508_K5FtU9wb9RK1kSpIgLozFCJc9DF6X4E8.jpg"
   },
   {
     title: "Eyelash Lift with Tint",
     price: "from $50",
     description: "Lifted and tinted lashes for a dramatic look",
     duration: "45 mins",
-    image: "https://media.istockphoto.com/photos/beauty-treatment-curling-eyelashes-by-using-a-curler-lash-lamination-picture-id1074129756?k=20&m=1074129756&s=612x612&w=0&h=ZhMKMFDf2QMWzfxgNXANoU2EfMQqD8oHYOThfzevwbA="
+    image: "https://media.istockphoto.com/id/1423975299/photo/making-makeup-in-a-beauty-salon-applying-black-mascara-to-the-eyelashes-with-a-makeup-brush.jpg?s=612x612&w=0&k=20&c=vdI3VBmWcWK3TZxG_ak7ala16u-SczCmI2cRyRSYYu8="
   },
   {
     title: "Eyebrow Henna Tattoo",
     price: "from $40",
     description: "Semi-permanent brow enhancement",
     duration: "40 mins",
-    image: "https://media.istockphoto.com/photos/when-words-cant-express-try-henna-picture-id1175414251?k=20&m=1175414251&s=612x612&w=0&h=OeS2QqzBtY1QkZ3eG4e4JTHi-SFkj3nW7De8R3-95Rg="
+    image: "https://media.istockphoto.com/id/637658768/photo/makeup-artist-applies-paint-henna-on-eyebrows.jpg?s=612x612&w=0&k=20&c=7vW73CS2dLguulkzDmPq6_WvdJdCn0t7-PQnznmaFVE="
   },
   {
     title: "Eyebrow Lamination with Tint",
     price: "from $65",
     description: "Restructured and tinted brows for a fuller look",
     duration: "60 mins",
-    image: "https://media.istockphoto.com/photos/closeup-photo-of-beautician-applying-dye-on-womans-eyebrows-stock-picture-id1219595404?k=20&m=1219595404&s=612x612&w=0&h=wcuXUOQKr1U_hxnSHeuXS-7eiQouwAc3raDogCMLGO8="
+    image: "https://t4.ftcdn.net/jpg/04/72/10/11/240_F_472101170_GdJpDB6CLoAupjDEYLrjH3Kvo0Mltyyw.jpg"
   }
 ];
