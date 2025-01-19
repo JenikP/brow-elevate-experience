c33e0d297762b03ab920636e931e5f401a4686e2
JenikP <95909702+JenikP@users.noreply.github.com>
2025-01-19 13:13:22 +1100

    Update facialTreatments.ts

--- src/data/services/facialTreatments.ts
+++ src/data/services/facialTreatments.ts
@@ -1,23 +1,26 @@
 export const facial = [
   {
     title: "Mini Facial",
-    price: "from $50",
     description: "Quick refreshing facial treatment",
-    duration: "30 mins",
     locationPrices: {
-      "location1": "from $50",
-      "location2": "from $55",
-      "location3": "from $60",
-      "location4": "from $65",
+      "location1": "from $70",
+      "location2": "Not Available",
+      "location3": "from $70",
+      "location4": "from $70",
       "location5": "from $70"
     },
     image: "https://images.unsplash.com/photo-1570172619644-dfd03ed5d881?auto=format&fit=crop&w=800&q=80"
   },
   {
     title: "Full Facial",
-    price: "from $80",
+    locationPrices: {
+      "location1": "from $85",
+      "location2": "Not Available",
+      "location3": "from $85",
+      "location4": "from $85",
+      "location5": "from $85"
+    },
     description: "Complete facial treatment for ultimate rejuvenation",
-    duration: "60 mins",
     image: "https://t3.ftcdn.net/jpg/08/74/09/20/240_F_874092052_tyNQTd1VKpLnMF8FjGGTQMSJgechEHSK.jpg"
   }
 ];
