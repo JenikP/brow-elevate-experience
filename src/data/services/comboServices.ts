0ab35382c39905fb649fbe0c208101f559573fb2
JenikP <95909702+JenikP@users.noreply.github.com>
2025-01-19 13:10:32 +1100

    Update comboServices.ts

--- src/data/services/comboServices.ts
+++ src/data/services/comboServices.ts
@@ -1,30 +1,48 @@
 export const comboOffers = [
   {
     title: "Eyebrow + Upper Lip",
-    price: "from $20",
     description: "Popular combo for complete face framing",
-    duration: "25 mins",
     locationPrices: {
       "location1": "from $20",
-      "location2": "from $22",
+      "location2": "from $20",
       "location3": "from $24",
       "location4": "from $26",
-      "location5": "from $28"
+      "location5": "from $22"
     },
     image: "https://t3.ftcdn.net/jpg/02/98/50/36/240_F_298503634_v997pWBDrDjCGdEYQdoXThR7o0tgFwn1.jpg"
   },
   {
     title: "Eyebrow Threading & Eyelash Tint",
-    price: "from $28",
+    locationPrices: {
+      "location1": "from $20",
+      "location2": "from $34",
+      "location3": "from $24",
+      "location4": "from $26",
+      "location5": "from $35"
+    },
     description: "Perfect combination for defined eyes",
-    duration: "35 mins",
+    image: "https://media.istockphoto.com/photos/beauty-treatment-curling-eyelashes-by-using-a-curler-lash-lamination-picture-id1074129756?k=20&m=1074129756&s=612x612&w=0&h=ZhMKMFDf2QMWzfxgNXANoU2EfMQqD8oHYOThfzevwbA="
+  },
+      title: "Eyebrow Threading & Eyebrow Tint",
+    locationPrices: {
+      "location1": "from $30",
+      "location3": "from $30",
+      "location4": "from $30",
+      "location5": "from $30"
+    },
+    description: "Perfect combination for defined eyes",
     image: "https://media.istockphoto.com/photos/beauty-treatment-curling-eyelashes-by-using-a-curler-lash-lamination-picture-id1074129756?k=20&m=1074129756&s=612x612&w=0&h=ZhMKMFDf2QMWzfxgNXANoU2EfMQqD8oHYOThfzevwbA="
   },
   {
     title: "Eyebrow Tint & Eyelash Tint",
-    price: "from $30",
+    locationPrices: {
+      "location1": "from $20",
+      "location2": "from $40",
+      "location3": "from $24",
+      "location4": "from $26",
+      "location5": "from $35"
+    },
     description: "Complete eye enhancement package",
-    duration: "35 mins",
     image: "https://media.istockphoto.com/photos/closeup-photo-of-beautician-applying-dye-on-womans-eyebrows-stock-picture-id1219595404?k=20&m=1219595404&s=612x612&w=0&h=wcuXUOQKr1U_hxnSHeuXS-7eiQouwAc3raDogCMLGO8="
   }
 ];
