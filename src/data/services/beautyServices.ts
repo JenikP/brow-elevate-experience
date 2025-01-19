e3ab39f6b4a3ec79e6c917208c424af89c5d38b1
JenikP <95909702+JenikP@users.noreply.github.com>
2025-01-19 13:00:08 +1100

    Update beautyServices.ts

--- src/data/services/beautyServices.ts
+++ src/data/services/beautyServices.ts
@@ -2,7 +2,6 @@ export const beautyAddOns = [
   {
     title: "Eyebrow Tint",
     description: "Enhanced color for fuller-looking brows",
-    duration: "15 mins",
     locationPrices: {
       "location1": "from $17",
       "location2": "from $15",
@@ -16,7 +15,6 @@ export const beautyAddOns = [
     title: "Eyelash Tint",
     price: "from $20",
     description: "Darker, more defined lashes",
-    duration: "20 mins",
     image: "https://t4.ftcdn.net/jpg/06/68/57/55/240_F_668575508_K5FtU9wb9RK1kSpIgLozFCJc9DF6X4E8.jpg"
   },
   {
@@ -29,21 +27,24 @@ export const beautyAddOns = [
       "location5": "from $65"
     },
     description: "Lifted and tinted lashes for a dramatic look",
-    duration: "45 mins",
     image: "https://media.istockphoto.com/id/1423975299/photo/making-makeup-in-a-beauty-salon-applying-black-mascara-to-the-eyelashes-with-a-makeup-brush.jpg?s=612x612&w=0&k=20&c=vdI3VBmWcWK3TZxG_ak7ala16u-SczCmI2cRyRSYYu8="
   },
   {
     title: "Eyebrow Henna Tattoo",
     price: "from $40",
     description: "Semi-permanent brow enhancement",
-    duration: "40 mins",
     image: "https://media.istockphoto.com/id/637658768/photo/makeup-artist-applies-paint-henna-on-eyebrows.jpg?s=612x612&w=0&k=20&c=7vW73CS2dLguulkzDmPq6_WvdJdCn0t7-PQnznmaFVE="
   },
   {
     title: "Eyebrow Lamination with Tint",
-    price: "from $65",
+       locationPrices: {
+      "location1": "from $65",
+      "location2": "from $70",
+      "location3": "from $65",
+      "location4": "from $65",
+      "location5": "from $65"
+    },
     description: "Restructured and tinted brows for a fuller look",
-    duration: "60 mins",
     image: "https://t4.ftcdn.net/jpg/04/72/10/11/240_F_472101170_GdJpDB6CLoAupjDEYLrjH3Kvo0Mltyyw.jpg"
   }
 ];
