15d1eedfcafb86fc7033e20cd3bac59a6028c5a9
JenikP <95909702+JenikP@users.noreply.github.com>
2025-03-16 20:16:41 +1100

    Update comboServices.ts

--- src/data/services/comboServices.ts
+++ src/data/services/comboServices.ts
@@ -3,10 +3,10 @@ export const comboOffers = [
     title: "Eyebrow + Upper Lip",
     description: "Popular combo for complete face framing",
     locationPrices: {
-      "location1": "from $20",
+      "location1": "from $22",
       "location2": "from $20",
-      "location3": "from $24",
-      "location4": "from $26",
+      "location3": "from $22",
+      "location4": "from $22",
       "location5": "from $22"
     },
     image: "https://t3.ftcdn.net/jpg/02/98/50/36/240_F_298503634_v997pWBDrDjCGdEYQdoXThR7o0tgFwn1.jpg"
@@ -15,10 +15,10 @@ export const comboOffers = [
     title: "Eyebrow Threading & Eyelash Tint",
     description: "Perfect combination for defined eyes",
     locationPrices: {
-      "location1": "from $20",
+      "location1": "from $35",
       "location2": "from $34",
-      "location3": "from $24",
-      "location4": "from $26",
+      "location3": "from $35",
+      "location4": "from $35",
       "location5": "from $35"
     },
     image: "https://media.istockphoto.com/photos/beauty-treatment-curling-eyelashes-by-using-a-curler-lash-lamination-picture-id1074129756?k=20&m=1074129756&s=612x612&w=0&h=ZhMKMFDf2QMWzfxgNXANoU2EfMQqD8oHYOThfzevwbA="
@@ -39,10 +39,10 @@ export const comboOffers = [
     title: "Eyebrow Tint & Eyelash Tint",
     description: "Complete eye enhancement package",
     locationPrices: {
-      "location1": "from $20",
+      "location1": "from $35",
       "location2": "from $40",
-      "location3": "from $24",
-      "location4": "from $26",
+      "location3": "from $35",
+      "location4": "from $35",
       "location5": "from $35"
     },
     image: "https://media.istockphoto.com/photos/closeup-photo-of-beautician-applying-dye-on-womans-eyebrows-stock-picture-id1219595404?k=20&m=1219595404&s=612x612&w=0&h=wcuXUOQKr1U_hxnSHeuXS-7eiQouwAc3raDogCMLGO8="
