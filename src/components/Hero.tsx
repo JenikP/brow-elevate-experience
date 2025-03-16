d8e9b2a7bd45ac001a0eadb473d3e1e0eb10a090
JenikP <95909702+JenikP@users.noreply.github.com>
2025-03-16 20:44:17 +1100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
 
   return (
     <div className="relative min-h-screen flex items-center bg-pearl">
-      <div className="absolute inset-0 bg-[url('https://media.istockphoto.com/id/1405867120/photo/the-makeup-artist-plucks-the-eyebrows-after-the-coloring-procedure-professional-make-up-and.jpg?s=612x612&w=0&k=20&c=5I7lDHYvWflC0HsETVUoh36GP-q7B9E-o3OKOlJAsCI=')] bg-cover bg-center opacity-10"></div>
+      <div className="absolute inset-0 bg-[url('https://media.istockphoto.com/id/1325618971/photo/woman-in-gloves-applying-eyebrow-tattoo.jpg?s=612x612&w=0&k=20&c=p09XB77awV3f6k6_5H7fQjb4RMpbCNUEWhwN1XNWlYw=')] bg-cover bg-center opacity-10"></div>
       
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 relative">
         <div className="max-w-3xl animate-fade-up">
